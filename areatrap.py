a08bc34f06083ac5f9099f3f2de7453143963e40
Azatbraza <132365947+Azatbraza@users.noreply.github.com>
2026-02-28 15:24:34 +0500

    Add files via upload

---
+++ areatrap.py
@@ -0,0 +1,7 @@
+height = 5
+base1 = 5
+base2 = 6
+
+area = 0.5 * height * (base1 + base2)
+
+print("Expected Output:", area)
--- json.py
+++ json.py
@@ -1,27 +1,21 @@
 import json
 
-data = {
-    "name": "Azat",
-    "age": 18,
-    "is_student": True
-}
 
-json_string = json.dumps(data)
-print(json_string)
-
-
-data = {"a": 1, "b": 2}
-
-print(json.dumps(data, separators=(',', ':')))
-
-
-with open("data.json", "r") as f:
+with open("sample-data.json", "r") as f:
     data = json.load(f)
 
-print(data)
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':50} {'Description':20} {'Speed':8} {'MTU':6}")
+print("-" * 50 + " " + "-" * 20 + " " + "-" * 8 + " " + "-" * 6)
 
 
-data = {"course": "Python", "level": "Intermediate"}
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
 
-with open("output.json", "w") as f:
-    json.dump(data, f, indent=4)
+    dn = attributes.get("dn", "")
+    descr = attributes.get("descr", "")
+    speed = attributes.get("speed", "")
+    mtu = attributes.get("mtu", "")
+
+    print(f"{dn:50} {descr:20} {speed:8} {mtu:6}")
